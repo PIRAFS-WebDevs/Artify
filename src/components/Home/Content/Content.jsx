dd18681ea113b1a77dc6eb7d242cd278c9538944
Abu Taher <writerabutaher@gmail.com>
2023-11-02 20:32:00 +0600

    card

--- src/components/Home/Content/Content.jsx
+++ src/components/Home/Content/Content.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import Card from "./Card/Card";
 
 const Content = () => {
-    return (
-        <section>
-            content
-        </section>
-    );
+  return (
+    <section className="px-2 py-4">
+      <Card />
+    </section>
+  );
 };
 
 export default Content;
