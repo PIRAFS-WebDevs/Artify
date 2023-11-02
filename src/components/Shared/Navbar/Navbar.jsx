3c067b562f72b05545ccef3bab17d9568f3a0ccb
Abu Taher <writerabutaher@gmail.com>
2023-11-02 19:09:49 +0600

    navbar

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { FaMoon, FaUserCircle, FaShoppingBag } from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-        <nav>
-            nav
-        </nav>
-    );
+  return (
+    <nav className="flex justify-between items-center bg-[#252525] px-6 py-4 text-white">
+      <div className="flex gap-4 items-center">
+        <AiOutlineMenu size={"1.5rem"} />
+        <h1 className="text-xl font-medium">WareSun</h1>
+      </div>
+      <div className="flex gap-6 items-center">
+        <AiOutlineSearch size={"1.5rem"} className="hidden md:block" />
+        <FaMoon />
+        <FaShoppingBag size={"1.2rem"} className="hidden md:block" />
+        <button className="bg-[#00b482] text-white px-8 py-2 rounded-md hidden md:block">
+          Sing In
+        </button>
+        <FaUserCircle size={"1.5rem"} />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
