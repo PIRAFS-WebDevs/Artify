47e4c53f086ffa595c6ab63a91ca1629ddf4f66b
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2023-11-02 22:05:50 +0600

    conflige solve

--- src/app/(primaryLayout)/(Home)/layout.js
+++ src/app/(primaryLayout)/(Home)/layout.js
@@ -3,16 +3,11 @@ import Sidebar from "@/components/Shared/Sidebar/Sidebar";
 
 const layout = ({ children }) => {
   return (
-
-    <main className="min-h-[100vh] bg-darkPrimary relative ">
-
+    <main className="min-h-screen bg-darkPrimary relative ">
       <Navbar />
       <section className="grid grid-cols-6">
         <Sidebar />
-        <div className="col-span-5">
-
-        {children}
-        </div>
+        <div className="col-span-5">{children}</div>
       </section>
     </main>
   );
