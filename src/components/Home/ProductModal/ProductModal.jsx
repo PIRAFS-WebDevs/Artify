cd32445b999ca87c8454369216d287a242009f60
Abu Taher <writerabutaher@gmail.com>
2023-11-08 10:26:44 +0600

    color variable

--- src/components/Home/ProductModal/ProductModal.jsx
+++ src/components/Home/ProductModal/ProductModal.jsx
@@ -1,31 +1,34 @@
 import { motion as m, AnimatePresence } from "framer-motion";
+import CloseButton from "./CloseButton";
 
 const ProductModal = ({ isShow, setIsShow }) => {
   return (
     <>
-      {isShow && (
-        <AnimatePresence>
+      <AnimatePresence>
+        {isShow && (
           <m.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        onClick={() => setIsShow(false)}
-        className="fixed inset-0 z-50 grid cursor-pointer bg-slate-900/20 backdrop-blur place-items-center"
-      >
-        <m.div
-          initial={{ scale: 0, rotate: "12.5deg" }}
-          animate={{ scale: 1, rotate: "0deg" }}
-          exit={{ scale: 0, rotate: "0deg" }}
-          onClick={(e) => e.stopPropagation()}
-          className="bg-[#2a2a2a] text-white p-12 rounded-lg w-[1900px] h-[1070px] shadow-xl cursor-default relative"
-        >
-
-
-
-        </m.div>
-      </m.div>
-        </AnimatePresence>
-      )}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            onClick={() => setIsShow(false)}
+            className="fixed inset-0 z-50 grid cursor-pointer bg-slate-900/20 backdrop-blur place-items-center"
+          >
+            <m.div
+              initial={{ scale: 0, rotate: "12.5deg" }}
+              animate={{ scale: 1, rotate: "0deg" }}
+              exit={{ scale: 0, rotate: "0deg" }}
+              onClick={(e) => e.stopPropagation()}
+              className="bg-darkSec text-white p-12 rounded-lg shadow-xl relative"
+            >
+              <div className="">
+                <div className="flex justify-end w-full">
+                  <CloseButton setIsShow={setIsShow} />
+                </div>
+              </div>
+            </m.div>
+          </m.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
