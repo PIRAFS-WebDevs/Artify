394f41cd49890da157a99399c70dded0f8e869b2
Tahsin <gazitahin323@gmail.com>
2023-11-08 13:28:01 +0600

    product footer

--- src/components/Shared/Footer/HomeFooter.jsx
+++ src/components/Shared/Footer/HomeFooter.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+"use client"
+import React, { useContext } from "react";
 import footerdata from "@/data/footerData";
+import AllStateContext from "@/context/AllStateContext";
 
 const HomeFooter = () => {
+  const { sideBarOpen, setSideBarOpen } = useContext(AllStateContext);
+
   return (
-    <div className="grid sm:grid-cols-1 mt-auto text-sm py-8  dark:bg-darkPrimary md:grid-cols-2 lg:grid-cols-5 gap-10 ">
-      <div className="col-span-1"></div>
+
+    <div className={`grid sm:grid-cols-1 mt-auto text-sm py-8  dark:bg-darkPrimary md:grid-cols-2 lg:grid-cols-4 ${sideBarOpen ? "ml-[300px]" : "ml-[150px]"} gap-10 `}>
+    {/* {sideBarOpen?<div className="col-span-1"></div> : <div className="col-s"></div>} */}
+      
 
       {footerdata.map((categoryObj, index) => (
         <div key={index} className=" text-white">
