aeba786f7d4940c333ad9a1a74b4caf5af0c525b
Abu Taher <writerabutaher@gmail.com>
2023-11-12 10:40:46 +0600

    settings layout

---
+++ src/components/Settings/Sidebar.jsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 w-[365px] border-r border-dark-200">
+      <div>
+        <p>Profile</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
