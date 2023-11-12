35c71cc676891331928d8ff055bbeb8e47a4fe5f
Abu Taher <writerabutaher@gmail.com>
2023-11-12 18:39:51 +0600

    sidebar responsive

--- src/app/(settings)/profile/page.js
+++ src/app/(settings)/profile/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Profile from "@/components/Settings/Profile";
+import Profile from "@/components/Settings/Profile/Profile";
 
 const ProfilePage = () => {
   return (
