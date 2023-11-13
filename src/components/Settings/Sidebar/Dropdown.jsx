da70ba4652e5b68cf31182fc1afcbe190fb90993
Abu Taher <writerabutaher@gmail.com>
2023-11-13 10:37:06 +0600

    settings sidebar responsive

--- src/components/Settings/Sidebar/Dropdown.jsx
+++ src/components/Settings/Sidebar/Dropdown.jsx
@@ -1,65 +1,22 @@
 "use client";
 
-import {
-  FiEdit,
-  FiChevronDown,
-  FiTrash,
-  FiShare,
-  FiPlusSquare,
-} from "react-icons/fi";
+import { FiChevronDown } from "react-icons/fi";
 import { motion } from "framer-motion";
 import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-// const Dropdown = () => {
-//   const [open, setOpen] = useState(false);
-
-//   return (
-//     <div className="flex items-center justify-center text-dark-100">
-//       <motion.div animate={open ? "open" : "closed"} className="relative">
-//         <button
-//           onClick={() => setOpen((pv) => !pv)}
-//           className="p-3 rounded text-dark-100 border border-dark-200"
-//         >
-//           <span className="text-sm">Profile</span>
-//           <motion.span variants={iconVariants}>
-//             <FiChevronDown />
-//           </motion.span>
-//         </button>
-
-//         <motion.ul
-//           initial={wrapperVariants.closed}
-//           variants={wrapperVariants}
-//           style={{ originY: "top", translateX: "-50%" }}
-//           className="flex flex-col gap-2 p-2 rounded bg-dark-200 shadow absolute top-[120%] left-[-100%] w-48 overflow-hidden"
-//         >
-//           {settingsSidebarData.map((e, i) => (
-//             <Link
-//               href={`${e.path}`}
-//               onClick={() => setOpen(false)}
-//               className="flex items-center gap-2 w-full p-2 text-xs rounded hover:bg-dark-300 text-white transition-all cursor-pointer"
-//             >
-//               {e.icon}
-//               <span>{e.name}</span>
-//             </Link>
-//           ))}
-//         </motion.ul>
-//       </motion.div>
-//     </div>
-//   );
-// };
+import { settingsSidebarData } from "@/data/SettingsSidebarData";
 
 const DropDown = () => {
   const [open, setOpen] = useState(false);
   const pathname = usePathname();
 
   return (
-    <div className="">
-      <motion.div animate={open ? "open" : "closed"} className="relative">
+    <div className="px-4 py-2">
+      <motion.div animate={open ? "open" : "closed"} className="relative z-10">
         <button
           onClick={() => setOpen((pv) => !pv)}
-          className="flex items-center gap-2 px-3 py-2 rounded-md text-indigo-50 bg-indigo-500 hover:bg-indigo-500 transition-colors"
+          className="flex items-center justify-between py-2 px-4 rounded-md text-white bg-transparent border border-dark-100 transition-all w-full"
         >
           <span className="font-medium text-sm capitalize">
             {pathname.slice(1)}
@@ -73,30 +30,27 @@ const DropDown = () => {
           initial={wrapperVariants.closed}
           variants={wrapperVariants}
           style={{ originY: "top", translateX: "-50%" }}
-          className="flex flex-col gap-2 p-2 rounded-lg bg-white shadow-xl absolute top-[120%] left-[50%] w-48 overflow-hidden"
+          className="flex flex-col gap-2 p-2 rounded bg-dark-400 text-white border border-dark-100 shadow absolute top-[120%] left-[50%] w-full overflow-hidden"
         >
-          <Option setOpen={setOpen} Icon={FiEdit} text="Edit" />
-          <Option setOpen={setOpen} Icon={FiPlusSquare} text="Duplicate" />
-          <Option setOpen={setOpen} Icon={FiShare} text="Share" />
-          <Option setOpen={setOpen} Icon={FiTrash} text="Remove" />
+          {settingsSidebarData.map((e, i) => (
+            <Link href={`${e.path}`} key={i}>
+              <motion.li
+                variants={itemVariants}
+                onClick={() => setOpen(false)}
+                className={`flex items-center gap-2 w-full p-2 text-sm font-medium whitespace-nowrap rounded hover:bg-dark-200 text-dark-100 hover:text-white transition-all cursor-pointer ${
+                  pathname === e.path && "text-white bg-dark-200"
+                }`}
+              >
+                <motion.span variants={actionIconVariants}>
+                  {e.icon}
+                </motion.span>
+                <span>{e.name}</span>
+              </motion.li>
+            </Link>
+          ))}
         </motion.ul>
       </motion.div>
     </div>
-  );
-};
-
-const Option = ({ text, Icon, setOpen }) => {
-  return (
-    <motion.li
-      variants={itemVariants}
-      onClick={() => setOpen(false)}
-      className="flex items-center gap-2 w-full p-2 text-xs font-medium whitespace-nowrap rounded-md hover:bg-indigo-100 text-slate-700 hover:text-indigo-500 transition-colors cursor-pointer"
-    >
-      <motion.span variants={actionIconVariants}>
-        <Icon />
-      </motion.span>
-      <span>{text}</span>
-    </motion.li>
   );
 };
 
@@ -105,17 +59,17 @@ export default DropDown;
 const wrapperVariants = {
   open: {
     scaleY: 1,
-    transition: {
-      when: "beforeChildren",
-      staggerChildren: 0.1,
-    },
+    // transition: {
+    //   when: "beforeChildren",
+    //   staggerChildren: 0.1,
+    // },
   },
   closed: {
     scaleY: 0,
-    transition: {
-      when: "afterChildren",
-      staggerChildren: 0.1,
-    },
+    // transition: {
+    //   when: "afterChildren",
+    //   staggerChildren: 0.1,
+    // },
   },
 };
 
@@ -127,17 +81,17 @@ const iconVariants = {
 const itemVariants = {
   open: {
     opacity: 1,
-    y: 0,
-    transition: {
-      when: "beforeChildren",
-    },
+    // y: 0,
+    // transition: {
+    //   when: "beforeChildren",
+    // },
   },
   closed: {
     opacity: 0,
-    y: -15,
-    transition: {
-      when: "afterChildren",
-    },
+    // y: -15,
+    // transition: {
+    //   when: "afterChildren",
+    // },
   },
 };
 
--- src/components/Settings/Sidebar/Sidebar.jsx
+++ src/components/Settings/Sidebar/Sidebar.jsx
@@ -9,24 +9,24 @@ const Sidebar = () => {
   const pathname = usePathname();
 
   return (
-    <div className="w-[280px] md:border-r border-dark-200">
-      {/* {settingsSidebarData.map((data, i) => {
+    <div className="w-full md:w-[280px] md:border-r border-dark-200">
+      {settingsSidebarData.map((data, i) => {
         const { name, icon, path } = data;
 
         return (
           <Link
             href={`${path}`}
             key={i}
-            className={`flex items-center gap-4 py-4 px-6 text-dark-100 hover:text-white cursor-pointer w-full select-none ${
-              pathname === path && "bg-dark-300"
+            className={`hidden md:flex items-center gap-4 py-4 px-6 text-dark-100 hover:text-white cursor-pointer w-full select-none ${
+              pathname === path && "bg-dark-300 text-white"
             }`}
           >
             {icon}
-            <p className="text-dark-100 text-sm">{name}</p>
+            <p className="text-sm">{name}</p>
           </Link>
         );
-      })} */}
-      <div>
+      })}
+      <div className="md:hidden block">
         <Dropdown />
       </div>
     </div>
