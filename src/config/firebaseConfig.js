b3ed3b4dec5dc6e8b1f46e6e0168b7417079a28a
Abu Taher <writerabutaher@gmail.com>
2023-11-15 00:40:32 +0600

    upload images into firebase

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,13 +1,31 @@
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
+// import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const storage = getStorage(app);
+// export default app;
+
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
+  apiKey: "AIzaSyBW31wKtWQjE0TxQHC6hPk1-GjQfuzwkoQ",
+  authDomain: "abu-taher-portfolio.firebaseapp.com",
+  projectId: "abu-taher-portfolio",
+  storageBucket: "abu-taher-portfolio.appspot.com",
+  messagingSenderId: "925281250133",
+  appId: "1:925281250133:web:2eddacabacd853a1d41ddc",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
 export default app;
