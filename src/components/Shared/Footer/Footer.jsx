a5b2b36ebf355544811acc6341a4dadfbe85561a
Abu Taher <writerabutaher@gmail.com>
2023-11-21 11:06:21 +0600

    footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,36 +1,25 @@
-import React, { useContext } from "react";
-import footerdata from "@/data/footerData";
+import footerData from "@/data/footerData";
 import { FaFacebook, FaPinterest } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import {
-  AiFillGithub,
-  AiFillInstagram,
-  AiFillLinkedin,
-  AiFillTwitterCircle,
-} from "react-icons/ai";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 import { BsYoutube } from "react-icons/bs";
-import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>
-      <div
-        className={`grid sm:grid-cols-1 mt-auto text-sm py-8 px-6 lg:grid-cols-2 xl:grid-cols-4 gap-10 `}
-      >
-        {/* {sideBarOpen?<div className="col-span-1"></div> : <div className="col-s"></div>} */}
-
-        {footerdata.map((categoryObj, index) => (
+    <div className="px-6 py-8 text-sm">
+      <div className="grid gap-8 pb-8 border-b lg:grid-cols-2 xl:grid-cols-4 border-dark-200">
+        {footerData.map((categoryObj, index) => (
           <div key={index} className=" dark:text-dark-100">
             {Object.keys(categoryObj).map((categoryName, categoryIndex) => (
-              <div key={categoryName}>
-                <h2 className="py-3 pl-3 text-base font-bold bg-dark-400 ">
+              <div key={categoryIndex}>
+                <h2 className="px-3 py-3 text-base font-semibold bg-dark-400">
                   {categoryName}
                 </h2>
-                <ul>
+                <ul className="px-3 space-y-2">
                   {categoryObj[categoryName].map((subcategory, subIndex) => (
                     <li
                       key={subIndex}
-                      className="py-1 pl-3 cursor-pointer hover:dark:text-white"
+                      className="cursor-pointer first:mt-2 hover:dark:text-white"
                     >
                       {subcategory.name}
                     </li>
@@ -40,8 +29,8 @@ const Footer = () => {
             ))}
           </div>
         ))}
-        <div className="mr-5 xl:col-start-4 lg:col-start-2">
-          <div className="flex gap-3 text-xl dark:text-dark-100">
+        <div className="px-3 xl:col-start-4 lg:col-start-2">
+          <div className="flex gap-2 text-xl dark:text-dark-100">
             <FaFacebook className="cursor-pointer hover:text-blue-500" />
             <FaSquareXTwitter className="cursor-pointer hover:text-dark-200" />
             <AiFillInstagram className="cursor-pointer hover:text-pink-600" />
@@ -52,23 +41,16 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="items-center justify-between mb-20 text-xs lg:flex m-14 md:mb-10 dark:text-dark-100">
-        <div className="items-center lg:flex">
-          <Image
-            src="/assets/logo/waresun.png"
-            height={50}
-            width={80}
-            priority={true}
-            alt="logo"
-            className="w-auto h-auto"
-          />
+      <div className="flex flex-col justify-between gap-2 pt-8 text-xs lg:flex-row dark:text-dark-100 mb-[60px] md:mb-0">
+        <div className="flex flex-col gap-2 lg:items-center lg:flex-row">
+          <img src="/assets/logo/waresun.png" alt="logo" className="w-32" />
           <p>© The waresun Limited. 2017-2023</p>
         </div>
         <div className="flex items-center gap-2">
           <button>Refunds</button>
           <button>Memberships</button>
           <button>Payment</button>
-          <button>Authorizeds</button>
+          <button>Authorized</button>
         </div>
       </div>
     </div>
