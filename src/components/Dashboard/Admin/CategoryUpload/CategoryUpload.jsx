ac39501ff64463d9546a670ae03074aa0331bd24
Abu Taher <writerabutaher@gmail.com>
2023-11-30 17:26:49 +0600

    cetegory done

--- src/components/Dashboard/Admin/CategoryUpload/CategoryUpload.jsx
+++ src/components/Dashboard/Admin/CategoryUpload/CategoryUpload.jsx
@@ -3,21 +3,65 @@
 import { useForm } from "react-hook-form";
 import UploadButton from "./UploadButton";
 import Description from "./Description";
-import { saveCategory } from "@/utils/api/category";
+import {
+  getCategoryById,
+  getCategoryId,
+  saveCategory,
+  updateCategory,
+} from "@/utils/api/category";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const CategoryUpload = () => {
+  const [singleCategory, setSingleCategory] = useState([]);
+  const searchParams = useSearchParams();
+  const updateId = searchParams.get("id");
+  const router = useRouter();
   const {
     register,
     handleSubmit,
+    setValue,
     reset,
     formState: { errors },
   } = useForm();
+  useEffect(() => {
+    if (singleCategory) {
+      setValue("name", singleCategory.name || "");
+      setValue("slug", singleCategory.slug || "");
+      setValue("details", singleCategory.details || "");
+    }
+  }, [singleCategory, setValue]);
 
-  const formHandler = (data) => {
-    saveCategory(data);
-    console.table(data);
+  const formHandler = async (data) => {
+    if (!updateId) {
+      const res = await saveCategory(data);
+      console.log("saveRes:", res);
+      console.table(data);
+      if (res.data.success) {
+        toast.success("layout is uploaded");
+        router.replace("/dashboard/admin/categories");
+      }
+    } else if (updateId) {
+      const res = await updateCategory(data, updateId);
+      console.log("updatedRes:", res);
+      if (res.data.success) {
+        toast.success("Category is updated");
+        router.replace("/dashboard/admin/categories");
+      }
+    }
   };
-
+  useEffect(() => {
+    (async () => {
+      if (!updateId) {
+        setSingleCategory([]);
+      }
+      if (updateId) {
+        const Category = await getCategoryById(updateId);
+        setSingleCategory(Category);
+      }
+    })();
+  }, [updateId]);
   return (
     <div className="divide-y divide-dark-200">
       <h1 className="pb-6 text-lg font-medium text-white">Category Upload</h1>
--- src/utils/api/category.js
+++ src/utils/api/category.js
@@ -10,12 +10,29 @@ export const saveCategory = async (data) => {
     console.error(error.message);
   }
 };
+export const updateCategory = async (data, id) => {
+  try {
+    const res = await api.patch(`/admin/category/category-update/${id}`, data);
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 // get category into database
 export const getCategory = async () => {
   try {
     const res = await api.get("/admin/category/all-category");
     console.log(res);
+    return res.data.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+export const getCategoryById = async (id) => {
+  try {
+    const res = await api.get(`/admin/layout/Single-category/${id}`);
     return res.data.data;
   } catch (error) {
     console.error(error.message);
