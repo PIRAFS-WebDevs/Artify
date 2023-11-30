8f30548b057bd81a83a7761284e8b130e47d2329
Abu Taher <writerabutaher@gmail.com>
2023-11-30 17:51:23 +0600

    tag updated done

--- src/components/Dashboard/Admin/TagUpload/TagUpload.jsx
+++ src/components/Dashboard/Admin/TagUpload/TagUpload.jsx
@@ -3,20 +3,60 @@
 import { useForm } from "react-hook-form";
 import UploadButton from "./UploadButton";
 import Description from "./Description";
-import { saveTag } from "@/utils/api/tags";
+import { getTagById, saveTag, updateTag } from "@/utils/api/tags";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const TagUpload = () => {
+  const [singleTag, setSingleTag] = useState([]);
+  const searchParams = useSearchParams();
+  const updateId = searchParams.get("id");
+  const router = useRouter();
   const {
     register,
     handleSubmit,
+    setValue,
     reset,
     formState: { errors },
   } = useForm();
+  useEffect(() => {
+    if (singleTag) {
+      setValue("name", singleTag.name || "");
+      setValue("slug", singleTag.slug || "");
+      setValue("details", singleTag.details || "");
+    }
+  }, [singleTag, setValue]);
 
-  const formHandler = (data) => {
-    saveTag(data);
-    console.table(data);
+  const formHandler = async (data) => {
+    if (!updateId) {
+      const res = await saveTag(data);
+      console.log("saveRes:", res);
+      console.table(data);
+      if (res?.data?.success) {
+        toast.success("Tags is uploaded");
+        router.replace("/dashboard/admin/tags");
+      }
+    } else if (updateId) {
+      const res = await updateTag(data, updateId);
+      console.log("updatedRes:", res);
+      if (res?.data?.success) {
+        toast.success("Tags is updated");
+        router.replace("/dashboard/admin/tags");
+      }
+    }
   };
+  useEffect(() => {
+    (async () => {
+      if (!updateId) {
+        setSingleTag([]);
+      }
+      if (updateId) {
+        const Category = await getTagById(updateId);
+        setSingleTag(Category);
+      }
+    })();
+  }, [updateId]);
 
   return (
     <div className="divide-y divide-dark-200">
--- src/utils/api/tags.js
+++ src/utils/api/tags.js
@@ -10,12 +10,29 @@ export const saveTag = async (data) => {
     console.error(error.message);
   }
 };
+export const updateTag = async (data, id) => {
+  try {
+    const res = await api.patch(`/admin/tags/tags-update/${id}`, data);
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 // get all tags
 export const getTags = async () => {
   try {
     const res = await api.get("/admin/tags/all-tags");
     console.log(res);
+    return res.data.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+export const getTagById = async (id) => {
+  try {
+    const res = await api.get(`/admin/layout/Single-tag/${id}`);
     return res.data.data;
   } catch (error) {
     console.error(error.message);
