b1c2b1a340bb69d5d32726c44e694c86d1c9621f
Abu Taher <writerabutaher@gmail.com>
2024-02-19 08:09:29 +0600

    Update QueryProvider.js and env

--- src/providers/QueryProvider.js
+++ src/providers/QueryProvider.js
@@ -1,10 +1,15 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
 
 const QueryProvider = ({ children }) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
