3a1de8a95731f6c3787bf8c59a70b33c06539b84
Abu Taher <writerabutaher@gmail.com>
2024-02-24 00:21:50 +0600

    firebase image upload

--- src/components/Dashboard/ProductUpload/FeatureImage.jsx
+++ src/components/Dashboard/ProductUpload/FeatureImage.jsx
@@ -1,11 +1,53 @@
+import { storage } from "@/config/firebaseConfig";
+import { Skeleton } from "@nextui-org/react";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
+import Image from "next/image";
+import { useState } from "react";
 import { FaCloudUploadAlt } from "react-icons/fa";
 
 const FeatureImage = ({ featuredImage, setFeaturedImage }) => {
-  const handleImage = (e) => {
-    const targetImage = e.target.files[0];
-    setFeaturedImage(targetImage);
+  const [loading, setLoading] = useState(false);
 
-    e.target.value = null;
+  const handleImage = async (event) => {
+    const selectedFile = event.target.files?.[0];
+
+    if (selectedFile) {
+      setLoading(true);
+
+      try {
+        const name = `${selectedFile.name}_${new Date().getTime()}`;
+        const imageRef = ref(storage, `images/${name}`);
+        const snapshot = await uploadBytes(imageRef, selectedFile);
+        const downloadURL = await getDownloadURL(snapshot.ref);
+
+        setFeaturedImage(downloadURL);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        console.error("Error uploading image:", error);
+      }
+    }
+  };
+
+  const handleRemoveImage = async () => {
+    if (featuredImage) {
+      try {
+        const filename = featuredImage.split("/").pop();
+        const decodedFile = decodeURIComponent(filename);
+        const imageName = decodedFile.split("?")[0];
+        const imageRef = ref(storage, imageName);
+        await deleteObject(imageRef);
+
+        setFeaturedImage(null);
+      } catch (error) {
+        console.error("Error deleting image:", error);
+      }
+    }
   };
 
   return (
@@ -34,14 +76,30 @@ const FeatureImage = ({ featuredImage, setFeaturedImage }) => {
             onChange={handleImage}
             id="dropzone-file"
             type="file"
+            accept="image/jpeg, image/png"
             className="hidden w-full"
           />
         </label>
         <div className="grid grid-cols-3 gap-4 md:grid-cols-5">
-          <img
-            src={featuredImage ? URL.createObjectURL(featuredImage) : null}
-            alt=""
-          />
+          {featuredImage && (
+            <div className="relative">
+              <Image
+                width={400}
+                height={400}
+                className="w-[10rem]"
+                src={featuredImage}
+                alt="featuredImage"
+              />
+              <span
+                onClick={handleRemoveImage}
+                className="absolute px-1.5 text-sm rounded-full cursor-pointer dark:text-white -top-2 -right-2 bg-primary"
+              >
+                x
+              </span>
+            </div>
+          )}
+
+          {loading && <Skeleton className="w-[8rem] h-[8rem] rounded" />}
         </div>
       </div>
     </div>
