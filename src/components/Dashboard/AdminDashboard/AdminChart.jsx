65fa36e691ba0359a54c35953c60ea5bc8bd19c0
Abu Taher <writerabutaher@gmail.com>
2024-05-23 11:51:12 +0600

    stats and chart

--- src/components/Dashboard/AdminDashboard/AdminChart.jsx
+++ src/components/Dashboard/AdminDashboard/AdminChart.jsx
@@ -10,82 +10,7 @@ import {
   YAxis,
 } from "recharts";
 
-const AdminChart = () => {
-  const data = [
-    {
-      name: "January",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "February",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "March",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "April",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "May",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "June",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: "July",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-    {
-      name: "August",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-    {
-      name: "September",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-    {
-      name: "October",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-    {
-      name: "November",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-    {
-      name: "December",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
-
+const AdminChart = ({ data }) => {
   return (
     <div
       className="lg:h-[500px] h-[300px] dark:bg-dark-400 bg-light-100 my-5
@@ -104,18 +29,23 @@ const AdminChart = () => {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="layout" />
           <YAxis />
           <Tooltip />
           <Legend />
           <Line
             type="monotone"
-            dataKey="pv"
+            dataKey="price"
+            name="Sales"
             stroke="#8884d8"
             activeDot={{ r: 8 }}
           />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-          <Line type="monotone" dataKey="amt" stroke="#e9ca9e" />
+          <Line
+            type="monotone"
+            name="Revenue"
+            dataKey="sale_price"
+            stroke="#82ca9d"
+          />
         </LineChart>
       </ResponsiveContainer>
     </div>
--- src/components/Dashboard/AdminDashboard/StatsCard.jsx
+++ src/components/Dashboard/AdminDashboard/StatsCard.jsx
@@ -1,18 +1,16 @@
-import { BiDollar } from "react-icons/bi";
-
-const StatsCard = ({ title, days, income }) => {
+const StatsCard = ({ title, days, income, icon }) => {
   return (
-    <div className="flex flex-col justify-between col-span-1 p-5 text-xs bg-light-100 border rounded-sm h-36 dark:border-dark-300 dark:bg-dark-400 md:text-base">
+    <div className="flex flex-col justify-between col-span-1 p-5 text-xs border rounded-sm bg-light-100 h-36 dark:border-dark-300 dark:bg-dark-400 md:text-base">
       <div className="flex justify-between ">
         <p>
           {title} <br />
           <span className="text-xs dark:text-dark-100">({days})</span>
         </p>
-        <div className="flex items-center justify-center w-8 h-8 rounded-full bg-primary md:w-14 md:h-14">
-          <BiDollar className="md:text-2xl" />
+        <div className="flex items-center justify-center w-8 h-8 text-xl rounded-full bg-primary md:w-14 md:h-14">
+          {icon}
         </div>
       </div>
-      <div>${income}</div>
+      <div className="text-lg">$ {income}</div>
     </div>
   );
 };
